bb2536c729f53c716e34ba92a9f877836161eafb
pedrophj <pedrophj@yahoo.com.br>
2021-02-01 23:40:35 -0300

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,14 @@
+import vrep 
+from pedrohj import *
+
+# Configuração (Setup)
+arduino = objeto()
+
+# Loop infinito
+while True:
+
+	print("Rodando...")	
+	arduino.delay(200)	
+	arduino.rodar()
+
+	
